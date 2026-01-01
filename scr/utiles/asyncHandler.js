092526b0d2375c43c4fc809cb90d705526b6bc79
asit singha <asit20singha02@gmail.com>
2026-01-01 22:31:07 +0530

    add register adn all bug are fixed

--- scr/utiles/asyncHandler.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler = (resquestHandler) => {
-	  return  (req, res, err) => {
-		Promise.resolve(resquestHandler(req, res, err)).catch((err) =>
-			console.log(err)
-		);
-	};
-};
-
-export { asyncHandler };
